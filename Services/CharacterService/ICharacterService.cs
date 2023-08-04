2b90c4b7e61b43a569da62b05e843bd9dd23e05f
h4lo343 <xiangnan1@student.unimelb.edu.au>
2023-08-04 16:51:10 +1000

    build up models& services

---
+++ Services/CharacterService/ICharacterService.cs
@@ -0,0 +1,12 @@
+﻿using dotnet_rpg.Dtos.Character;
+
+namespace dotnet_rpg.Services.CharacterService
+{
+    public interface ICharacterService
+    {
+        Task<ServiceResponse<List<GetCharacterDto>>> GetAllCharacters();
+        Task<ServiceResponse<GetCharacterDto>> GetCharacterById(int id);
+        Task<ServiceResponse<List<GetCharacterDto>>> AddCharacter(AddCharacterDto newCharacter); 
+
+    }
+}
